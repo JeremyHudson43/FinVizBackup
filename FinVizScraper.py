e5ea3e0c21089d7afbdfa17df95c89035151163c
JeremyHudson43 <jer4349@gmail.com>
2021-04-09 10:01:31 -0400

    upload scraper

---
+++ FinVizScraper.py
@@ -0,0 +1,219 @@
+from finviz.screener import Screener
+import os
+import datetime
+# BDay is business day, not birthday...
+from pandas.tseries.offsets import BDay
+
+today = datetime.datetime.today()
+last_business_day = (today - BDay(1)).date()
+
+folder_path = "C:\\Users\\Frank Einstein\\Desktop\\stock records"
+
+def small_mc_rel_vol():
+    # small market cap stocks with few shares outstanding
+    filters = ['f=cap_microunder','sh_float_u10','sh_outstanding_u10','sh_price_u15','sh_relvol_o2']
+    stock_list = Screener(filters=filters, table='Performance', order='price')
+
+    # Export the screener results to .csv
+    stock_list.to_csv(os.path.join(folder_path, ("small cap rel vol\\" + str(last_business_day) + ".csv")))
+
+def analysts_buy():
+    filters = ['f=an_recom_strongbuy','sh_price_u5','targetprice_a50']
+    stock_list = Screener(filters=filters, table='Performance', order='price')
+
+    # Export the screener results to .csv
+    stock_list.to_csv(os.path.join(folder_path, ("analysts buy\\" + str(last_business_day) + ".csv")))
+
+
+def bankruptcy_squeeze_candidates():
+    # small market cap stocks with few shares outstanding
+    filters = ['f=fa_pb_low','sh_short_o30']
+    stock_list = Screener(filters=filters, table='Performance', order='price')
+
+    # Export the screener results to .csv
+    stock_list.to_csv(os.path.join(folder_path, ("bankruptcy squeeze candidates\\" + str(last_business_day) + ".csv")))
+
+
+def bounce_at_moving_average():
+    # small market cap stocks with few shares outstanding
+    filters = ['f=sh_avgvol_o400','sh_curvol_o2000','sh_relvol_o1','ta_sma20_pa','ta_sma50_pb']
+    stock_list = Screener(filters=filters, table='Performance', order='price')
+
+    # Export the screener results to .csv
+    stock_list.to_csv(os.path.join(folder_path, ("bounce at moving average\\" + str(last_business_day) + ".csv")))
+
+
+def breaking_out():
+    # small market cap stocks with few shares outstanding
+    filters = ['f=fa_debteq_u1','fa_roe_o20','sh_avgvol_o100','ta_highlow50d_nh','ta_sma20_pa','ta_sma200_pa','ta_sma50_pa']
+    stock_list = Screener(filters=filters, table='Performance', order='price')
+
+    # Export the screener results to .csv
+    stock_list.to_csv(os.path.join(folder_path, ("breaking out\\" + str(last_business_day) + ".csv")))
+
+
+def buy_and_hold_value():
+    # small market cap stocks with few shares outstanding
+    filters = ['f=cap_microover','fa_curratio_o1.5','fa_estltgrowth_o10','fa_peg_o1,fa_roe_o15','ta_beta_o1.5','ta_sma20_pa']
+    stock_list = Screener(filters=filters, table='Performance', order='price')
+
+    # Export the screener results to .csv
+    stock_list.to_csv(os.path.join(folder_path, ("buy and hold value\\" + str(last_business_day) + ".csv")))
+
+
+def canslim():
+    # small market cap stocks with few shares outstanding
+    filters = [f'=fa_eps5years_o20','fa_epsqoq_o20','fa_epsyoy_o20','fa_sales5years_o20','fa_salesqoq_o20','sh_curvol_o200']
+    stock_list = Screener(filters=filters, table='Performance', order='price')
+
+    # Export the screener results to .csv
+    stock_list.to_csv(os.path.join(folder_path, ("canslim\\" + str(last_business_day) + ".csv")))
+
+
+def consistent_growth_bullish_trend():
+    # small market cap stocks with few shares outstanding
+    filters = ['f=fa_eps5years_pos','fa_epsqoq_o20','fa_epsyoy_o25','fa_epsyoy1_o15','fa_estltgrowth_pos',
+               'fa_roe_o15','sh_instown_o10','sh_price_o15','ta_highlow52w_a90h','ta_rsi_nos50']
+    stock_list = Screener(filters=filters, table='Performance', order='price')
+
+    # Export the screener results to .csv
+    stock_list.to_csv(os.path.join(folder_path, ("consistent growth bullish trend\\" + str(last_business_day) + ".csv")))
+
+
+def earnings_gap_up():
+    # small market cap stocks with few shares outstanding
+    filters = ['f=earningsdate_tomorrowafter','sh_avgvol_o400','sh_curvol_o50','sh_short_u25','ta_averagetruerange_o0.5,ta_gap_u2']
+    stock_list = Screener(filters=filters, table='Performance', order='price')
+
+    # Export the screener results to .csv
+    stock_list.to_csv(os.path.join(folder_path, ("earnings gap up\\" + str(last_business_day) + ".csv")))
+
+
+def high_earnings_growth():
+    # small market cap stocks with few shares outstanding
+    filters = ['f=fa_epsqoq_o25','fa_epsyoy_o25','fa_epsyoy1_o25','fa_salesqoq_o25','sh_avgvol_o400','ta_rsi_nos50','ta_sma200_pa']
+    stock_list = Screener(filters=filters, table='Performance', order='price')
+
+    # Export the screener results to .csv
+    stock_list.to_csv(os.path.join(folder_path, ("high earnings growth\\" + str(last_business_day) + ".csv")))
+
+
+def high_relative_volume():
+    # small market cap stocks with few shares outstanding
+    filters = ['f=fa_curratio_o1','fa_epsqoq_o15','fa_quickratio_o1','fa_salesqoq_o15','sh_avgvol_o400','sh_price_o5',
+               'sh_relvol_o1.5','ta_sma20_pa','ta_sma200_sb50','ta_sma50_sa200']
+    stock_list = Screener(filters=filters, table='Performance', order='price')
+
+    # Export the screener results to .csv
+    stock_list.to_csv(os.path.join(folder_path, ("high relative volume\\" + str(last_business_day) + ".csv")))
+
+
+def high_sales_growth():
+    # small market cap stocks with few shares outstanding
+    filters = ['f=fa_debteq_u0.5','fa_roe_o15','fa_sales5years_o20','fa_salesqoq_o20','sh_avgvol_o200','sh_instown_o60','sh_price_o5','sh_short_u5']
+    stock_list = Screener(filters=filters, table='Performance', order='price')
+
+    # Export the screener results to .csv
+    stock_list.to_csv(os.path.join(folder_path, ("high sales growth\\" + str(last_business_day) + ".csv")))
+
+
+def low_pe_value():
+    # small market cap stocks with few shares outstanding
+    filters = ['f=cap_smallunder','fa_pb_low','fa_pe_low,fa_peg_low','fa_roa_pos','fa_roe_pos','sh_price_o5']
+    stock_list = Screener(filters=filters, table='Performance', order='price')
+
+    # Export the screener results to .csv
+    stock_list.to_csv(os.path.join(folder_path, ("low pe value\\" + str(last_business_day) + ".csv")))
+
+
+def new_highs():
+    # small market cap stocks with few shares outstanding
+    filters = ['f=an_recom_buy','sh_price_u7','ta_change_u','ta_highlow20d_nh','ta_highlow50d_nh','ta_highlow52w_nh','ta_perf_dup']
+    stock_list = Screener(filters=filters, table='Performance', order='price')
+
+    # Export the screener results to .csv
+    stock_list.to_csv(os.path.join(folder_path, ("new highs\\" + str(last_business_day) + ".csv")))
+
+
+def oversold_reversal():
+    # small market cap stocks with few shares outstanding
+    filters = ['f=sh_price_o5','sh_relvol_o2','ta_change_u','ta_rsi_os30']
+    stock_list = Screener(filters=filters, table='Performance', order='price')
+
+    # Export the screener results to .csv
+    stock_list.to_csv(os.path.join(folder_path, ("oversold reversal\\" + str(last_business_day) + ".csv")))
+
+
+def oversold_upcoming_earnings():
+    # small market cap stocks with few shares outstanding
+    filters = ['f=cap_smallover','earningsdate_thismonth','fa_epsqoq_o15','fa_grossmargin_o20','sh_avgvol_o750','sh_curvol_o1000','ta_perf_52w10o','ta_rsi_nob50']
+    stock_list = Screener(filters=filters, table='Performance', order='price')
+
+    # Export the screener results to .csv
+    stock_list.to_csv(os.path.join(folder_path, ("oversold upcoming earnings\\" + str(last_business_day) + ".csv")))
+
+
+def potential_uptrend_from_lows():
+    # small market cap stocks with few shares outstanding
+    filters = ['f=sh_avgvol_o400','ta_pattern_channelup','ta_perf_1wdown']
+    stock_list = Screener(filters=filters, table='Performance', order='price')
+
+    # Export the screener results to .csv
+    stock_list.to_csv(os.path.join(folder_path, ("potential uptrend from lows\\" + str(last_business_day) + ".csv")))
+
+
+def short_squeeze():
+    # small market cap stocks with few shares outstanding
+    filters = ['f=sh_avgvol_o100','sh_instown_u50','sh_price_o2','sh_short_o15']
+    stock_list = Screener(filters=filters, table='Performance', order='price')
+
+    # Export the screener results to .csv
+    stock_list.to_csv(os.path.join(folder_path, ("short squeeze\\" + str(last_business_day) + ".csv")))
+
+
+def shorted_stocks():
+    # small market cap stocks with few shares outstanding
+    filters = ['f=cap_smallover','geo_usa','sh_avgvol_o500','sh_curvol_o500','sh_opt_optionshort','sh_price_o3','sh_relvol_o1','sh_short_high']
+    stock_list = Screener(filters=filters, table='Performance', order='price')
+
+    # Export the screener results to .csv
+    stock_list.to_csv(os.path.join(folder_path, ("shorted stocks\\" + str(last_business_day) + ".csv")))
+
+def sma_crossover():
+    # small market cap stocks with few shares outstanding
+    filters = ['f=fa_pe_profitable','sh_avgvol_o400','sh_relvol_o1','sh_short_low','ta_beta_o1','ta_sma50_cross20b']
+    stock_list = Screener(filters=filters, table='Performance', order='price')
+
+    # Export the screener results to .csv
+    stock_list.to_csv(os.path.join(folder_path, ("sma crossover\\" + str(last_business_day) + ".csv")))
+
+
+def undervalued_dividend_growth():
+    # small market cap stocks with few shares outstanding
+    filters = ['f=cap_largeover','fa_div_pos','fa_epsyoy1_o5','fa_estltgrowth_o5','fa_payoutratio_u50','fa_pe_u20','fa_peg_low']
+    stock_list = Screener(filters=filters, table='Performance', order='price')
+
+    # Export the screener results to .csv
+    stock_list.to_csv(os.path.join(folder_path, ("undervalued dividend growth\\" + str(last_business_day) + ".csv")))
+
+
+small_mc_rel_vol()
+analysts_buy()
+bankruptcy_squeeze_candidates()
+bounce_at_moving_average()
+breaking_out()
+buy_and_hold_value()
+canslim()
+consistent_growth_bullish_trend()
+earnings_gap_up()
+high_earnings_growth()
+high_relative_volume()
+low_pe_value()
+new_highs()
+oversold_reversal()
+oversold_upcoming_earnings()
+potential_uptrend_from_lows()
+short_squeeze()
+shorted_stocks()
+sma_crossover()
+undervalued_dividend_growth()
