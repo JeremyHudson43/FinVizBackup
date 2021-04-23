fd56b99ff9ceb5784a6bf67c5d6dbc874fb94dc4
JeremyHudson43 <jer4349@gmail.com>
2021-04-23 08:27:09 -0400

    Update RSIcrossover.py

--- RSIcrossover.py
+++ RSIcrossover.py
@@ -8,6 +8,7 @@ import datetime
 import finviz
 import csv
 import ctypes
+from dateutil import parser
 
 MessageBox = ctypes.windll.user32.MessageBoxW
 
@@ -17,11 +18,17 @@ last_business_day = (today - BDay(1)).date()
 # loop through low RSI unique folder and add all matching results for previous business day to text file list
 folder_path = "C:\\Users\\Frank Einstein\\Desktop\\stock records\\low rsi\\unique"
 cross_path = "C:\\Users\\Frank Einstein\\Desktop\\stock records\\cross above 30 RSI\\stocks below RSI 30.txt"
-cross_path_folder = "C:\\Users\\Frank Einstein\\Desktop\\stock records\\cross above 30 RSI"
+cross_path_folder = "C:\\Users\\Frank Einstein\\Desktop\\stock records\\cross above 30 RSI\\unique"
 
 df = pd.concat(map(pd.read_csv, glob.glob(os.path.join(folder_path, "*.csv"))))
 
+formattedLBD = str(last_business_day).split('-')
+proper_format = formattedLBD[1] + "/" + formattedLBD[2] + "/" + formattedLBD[0]
+
 df = df[df['RSI (14)'].between(24, 28)]
+
+df['Date'] = pd.to_datetime(df['Date'])
+df = df[df['Date'] == proper_format]
 
 
 def block_one():
@@ -45,17 +52,28 @@ def block_two():
         try:
             stock = finviz.get_stock(line.strip())
 
+            stock['Date'] = str(last_business_day)
+            stock['Ticker'] = line.strip()
+
             path = os.path.join(cross_path_folder, line.strip() + ".csv")
 
-            argument = str(line.strip() + " is breaking out with an RSI of " + stock['RSI (14)'])
-
             if float(stock['RSI (14)']) > 32:
-                MessageBox(None, argument, 'RSI Alert', 0)
-                with open(path, 'a+') as f:
-                    w = csv.DictWriter(f, stock.keys())
-                    w.writeheader()
-                    w.writerow(stock)
-                    f.close()
+                del [line]
+                if os.path.isfile(path):
+                    ticker_df = pd.read_csv(path)
+
+                    if not (ticker_df['Date'].str.contains(str(last_business_day)).any()):
+                        with open(path, 'a') as f:
+                            w = csv.DictWriter(f, stock.keys())
+                            w.writerow(stock)
+                            f.close()
+
+                if not os.path.isfile(path):
+                    with open(path, 'w+') as f:
+                        w = csv.DictWriter(f, stock.keys())
+                        w.writeheader()
+                        w.writerow(stock)
+                        f.close()
         except:
             print("error")
 
