30dcad4e0b35aeb8bf1195d1af72c3a328b71c69
JeremyHudson43 <jer4349@gmail.com>
2021-06-22 21:07:43 -0400

    Update CompareFolders.py

--- CompareFolders.py
+++ CompareFolders.py
@@ -1,10 +1,16 @@
 import os
+import os.path
 
-individual_folder = 'top gainers'
+individual_folder = 'analysts buy'
 
-full_filepath = f"C:\\Users\\Frank Einstein\\Desktop\\stock records\\{individual_folder}\\unique"
+full_filepath = f"C:\\Users\\Frank Einstein\\Desktop\\stock records archive\\stock records\\{individual_folder}\\unique"
 
-folder_path = "C:\\Users\\Frank Einstein\\Desktop\\stock records"
+folder_path = "C:\\Users\\Frank Einstein\\Desktop\\stock records archive\\stock records"
+
+# full_filepath = f"C:\\Users\\Frank Einstein\\Desktop\\stock records\\{individual_folder}\\unique"
+
+# folder_path = "C:\\Users\\Frank Einstein\\Desktop\\stock records"
+
 
 full_filepath = set(os.path.relpath(os.path.join(root, file), full_filepath) for root, _, files in os.walk(full_filepath) for file in files)
 
@@ -17,27 +23,53 @@ for root, dirs, files in os.walk(folder_path):
 
 length = (len(next(os.walk(folder_path))[1]))
 
-for x in file_list[length:]:
-    compare = set(os.path.relpath(os.path.join(root, file), x) for root, _, files in os.walk(x) for file in files)
 
-    being_compared = str(x).split("\\")[5]
-    being_compared_length = str(len(compare))
+def compare():
+    for x in file_list[length:]:
+        compare = set(os.path.relpath(os.path.join(root, file), x) for root, _, files in os.walk(x) for file in files)
 
-    common_stocks_length = str(len(compare & full_filepath))
-    base_comparison_length = (len(full_filepath))
+        # being_compared = str(x).split("\\")[6]
 
-    percentage_similarity = len(compare & full_filepath) / (len(full_filepath)) * 100
-    percentage_similarity = "{:.2f}".format(percentage_similarity)
+        being_compared = str(x).split("\\")[5]
+        being_compared_length = str(len(compare))
 
-    print(f"Current length of {individual_folder} is {base_comparison_length}")
+        common_stocks_length = str(len(compare & full_filepath))
+        base_comparison_length = (len(full_filepath))
 
-    print(f"Current length of {being_compared} is {being_compared_length}\n")
+        percentage_similarity = len(compare & full_filepath) / (len(full_filepath)) * 100
+        percentage_similarity = "{:.2f}".format(percentage_similarity)
 
-    print(f"Number of common stocks between {being_compared} and {individual_folder} is {common_stocks_length}\n")
+        print(f"Current length of {individual_folder} is {base_comparison_length}")
 
-    print(f"The common stocks in {individual_folder} and {being_compared} are\n{compare & full_filepath}")
+        print(f"Current length of {being_compared} is {being_compared_length}\n")
 
-    try:
-        print(f"\n{being_compared} contains {percentage_similarity}% of the stocks in {individual_folder}\n")
-    except Exception as err:
-        print(err)
+        print(f"Number of common stocks between {being_compared} and {individual_folder} is {common_stocks_length}\n")
+
+        print(f"The common stocks in {individual_folder} and {being_compared} are\n{compare & full_filepath}")
+
+        try:
+            print(f"\n{being_compared} contains {percentage_similarity}% of the stocks in {individual_folder}\n")
+        except Exception as err:
+            print(err)
+
+# compare()
+listOfFiles = []
+
+
+for (dirpath, dirnames, filenames) in os.walk(folder_path):
+    listOfFiles += [os.path.join(dirpath, file) for file in filenames]
+
+
+def number_of_occurences(stock):
+    print(stock)
+    count = 0
+    for dirpath, dirnames, filenames in os.walk(folder_path):
+        for filename in [f for f in filenames if f.startswith(stock)]:
+            count +=1
+            if count > 10:
+               break
+            print(os.path.join(dirpath, filename), count)
+
+
+for x in listOfFiles:
+    number_of_occurences(x.split("\\")[-1])
