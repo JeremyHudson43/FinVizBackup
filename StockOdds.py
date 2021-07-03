61d05c1e218b4b5da4dcd7c954e444832c1b69c7
JeremyHudson43 <jer4349@gmail.com>
2021-07-03 16:23:27 -0400

    Update StockOdds.py

--- StockOdds.py
+++ StockOdds.py
@@ -32,7 +32,6 @@ for stock_num in range(len(stocks)):
     percent_change = []
 
     try:
-        # input a change percentage
         # in final results dataframe, count how many times that change percentage occurred
         # From each of these percentages, find if the following day traded higher or lower
         # Find the percentage of the following day trading higher or lower
@@ -55,15 +54,57 @@ for stock_num in range(len(stocks)):
         close_list = df['Close'].tolist()
 
         for x in range(len(close_list) - 1):
-            percent_change.append(percent(close_list[x], close_list[x + 1]))
+            change = percent(close_list[x], close_list[x + 1])
+            change = "{:.1f}".format(change)
+
+            percent_change.append(change)
 
         percent_change.insert(0, 0)
         df['Percent Change'] = percent_change
 
-        df.to_csv("C:\\Users\\Frank Einstein\\Desktop\\" + str(stock) + ".csv")
+        change_var = 19.0
+
+        # check dataframe next day after change % and see if lower or higher, increment counter
+        # every time you see the specified %, then add up winners and losers to find percent
+
+        winners = 0
+        losers = 0
+
+        df_mask = pd.to_numeric(df['Percent Change']) == 4.0
+
+        ### NEXT DAY
+        filtered_df = df[df_mask]
+
+        index_list = filtered_df.index.tolist()
+        index_list_next_day = [x + 1 for x in index_list]
+
+        df_mask_prev_day = df.index.isin(index_list)
+        df_mask_next_day = df.index.isin(index_list_next_day)
+
+        filtered_df_prev_day = df[df_mask_prev_day]
+        filtered_df_next_day = df[df_mask_next_day]
+
+        print(filtered_df_next_day.head())
+        print(filtered_df_prev_day.head())
+
+        only_close_prev_day = filtered_df_prev_day['Close'].tolist()
+        only_close_next_day = filtered_df_next_day['Close'].tolist()
+
+        for price_num in range(len(only_close_next_day)):
+            if only_close_prev_day[price_num] > only_close_next_day[price_num]:
+                losers+=1
+            else:
+                winners+=1
+
+       #  print(percent(winners, losers))
+
+        # print(len(filtered_df['Percent Change']))
+
+        filtered_df.to_csv("C:\\Users\\Frank Einstein\\Desktop\\AAPL.csv")
 
     except Exception as err:
         print(err)
+
 
 
 
