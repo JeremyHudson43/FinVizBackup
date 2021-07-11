b17be76b33bc166d05c75849ee8dc70243f4c80a
JeremyHudson43 <jer4349@gmail.com>
2021-07-11 16:02:17 -0400

    Update Preprocess.py

--- Preprocess.py
+++ Preprocess.py
@@ -5,12 +5,9 @@ import functools
 import pandas as pd
 import glob
 
-folder_path = "C:\\Users\\Frank Einstein\\Desktop\\stock records archive\\stock records\\all stocks"
+folder_path = "C:\\Users\\Frank Einstein\\Desktop\\stock records"
 
-storage_path = "C:\\Users\\Frank Einstein\\Desktop\\stock records archive\\stock records\\combined CSVs"
-
-df = pd.concat(map(functools.partial(pd.read_csv, encoding='latin-1', compression=None, error_bad_lines=False),
-                   glob.glob(folder_path + "\\unique" + "/*.csv")))
+storage_path = "C:\\Users\\Frank Einstein\\Desktop\\stock records\\combined CSVs"
 
 def value_to_float(x):
     if type(x) == float or type(x) == int:
@@ -56,8 +53,7 @@ def clean_data(df, folder):
                      'SMA200', 'Change', 'Target Price', 'P/S', 'Short Ratio', 'Book/sh', 'Cash/sh', 'P/C', 'Dividend',
                      'P/FCF', 'Beta', 'Quick Ratio', 'ATR', 'Current Ratio', 'Price', 'Recom', 'P/E', 'RSI (14)', 'Forward P/E']
 
-    df = df.drop(['Company', 'Sector', 'Industry', 'Country', 'Volatility', 'Optionable', 'Shortable', 'Earnings', 'Date',
-                  'Date', 'News', '52W Range', 'Index'], axis=1)
+    df = df.drop(['Company', 'Sector', 'Industry', 'Country', 'Volatility', 'Optionable', 'Shortable', 'Date', 'Earnings', 'News', '52W Range', 'Index'], axis=1)
 
     # remove % from all specified columns to get the raw value
     df[cols_to_check] = df[cols_to_check].replace({'%':''}, regex=True)
@@ -71,4 +67,26 @@ def clean_data(df, folder):
     df.to_csv(os.path.join(storage_path, f"{folder}.csv"))
 
 
-clean_data(df, "all stocks")
+file_list = []
+
+for root, dirs, files in os.walk(folder_path):
+    for name in dirs:
+        path = os.path.join(root, name)
+        file_list.append(path)
+
+length = (len(next(os.walk(folder_path))[1]))
+
+# extract tickers from scraped mass CSVs to generate individual stock CSVs
+for unique_path in file_list[length:]:
+
+    print(unique_path)
+
+    try:
+        df = pd.concat(map(functools.partial(pd.read_csv, encoding='latin-1', compression=None, error_bad_lines=False),
+                           glob.glob(unique_path  + "/*.csv")))
+    except Exception as err:
+        print(err)
+
+    if "unique" in unique_path:
+        to_save = unique_path.split("\\")[-2]
+        clean_data(df, to_save)
