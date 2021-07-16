1455c873b5ae0d5d7a6d88b6f99e35d866320fb2
JeremyHudson43 <jer4349@gmail.com>
2021-07-16 07:26:43 -0400

    Update AppendPrices.py

--- AppendPrices.py
+++ AppendPrices.py
@@ -1,40 +1,70 @@
 import pandas as pd
 import yfinance as yf
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
 
 stock = "GPL"
 
-df_one = pd.read_csv("C:\\Users\\Frank Einstein\\Desktop\\stock records\\all stocks\\unique\\" + stock + ".csv")
-
-df_two = pd.read_csv("C:\\Users\\Frank Einstein\\Desktop\\stock history\\" + str(stock) + ".csv")
-
-sma_200 = df_two['Close'].tolist()[-200:]
-sma_200 = sum(sma_200) / len(sma_200)
-
-sma_20 = df_two['Close'].tolist()[-20:]
-sma_20 = sum(sma_20) / len(sma_20)
-
-sma_50 = df_two['Close'].tolist()[-50:]
-sma_50 = sum(sma_50) / len(sma_50)
+sma_200_list = []
+sma_20_list = []
+sma_50_list = []
 
 
 stock_to_save = yf.Ticker(stock)
 
+df_one = pd.read_csv("C:\\Users\\Frank Einstein\\Desktop\\stock records\\all stocks\\unique\\" + stock + ".csv")
+
 dates = df_one['Date'].tolist()
 
+start_date_one = datetime.strptime(dates[0], "%Y-%m-%d").date()
+start_date_two = datetime.strptime(dates[-1], "%Y-%m-%d").date()
+
 # get historical market data
-hist = stock_to_save.history(start=dates[0], end=dates[-1])
+hist_one = stock_to_save.history(start=dates[0], end=start_date_two + relativedelta(days=+2))
 
-hist_close = [x for x in hist['Close']]
-hist_open = [x for x in hist['Open']]
+# get historical market data
+hist_two = stock_to_save.history(start=start_date_two - relativedelta(months=+12), end=dates[-1])
 
-hist_low = [x for x in hist['Low']]
-hist_high = [x for x in hist['High']]
+
+for date_index in range(len(hist_one)):
+    sma_200 = hist_two['Close'].tolist()[-200 - date_index:]
+    sma_200 = sum(sma_200) / len(sma_200)
+    sma_200_list.append(sma_200)
+
+    sma_20 = hist_two['Close'].tolist()[-20 - date_index:]
+    sma_20 = sum(sma_20) / len(sma_20)
+    sma_20_list.append(sma_20)
+
+    sma_50 = hist_two['Close'].tolist()[-50- date_index :]
+    sma_50 = sum(sma_50) / len(sma_50)
+    sma_50_list.append(sma_50)
+
+    print(sma_200, sma_50, sma_20)
+
+sma_20_list.reverse()
+sma_50_list.reverse()
+sma_200_list.reverse()
+
+
+df_one['SMA20'] = pd.Series(sma_20_list)
+df_one['SMA50'] = pd.Series(sma_50_list)
+df_one['SMA200'] = pd.Series(sma_200_list)
+
+dates = df_one['Date'].tolist()
+
+
+hist_close = [x for x in hist_one['Close']]
+hist_open = [x for x in hist_one['Open']]
+
+hist_low = [x for x in hist_one['Low']]
+hist_high = [x for x in hist_one['High']]
 
 
 df_one['Close'] = pd.Series(hist_close)
 df_one['Open'] = pd.Series(hist_open)
 df_one['Low'] = pd.Series(hist_low)
 df_one['High'] = pd.Series(hist_high)
+
 
 df_one = df_one.drop_duplicates(subset=['Date'])
 
