eca36e3848dd44139f223b2b06cd05a7230c313d
JeremyHudson43 <jer4349@gmail.com>
2021-07-17 16:20:46 -0400

    Update IsolateTrend.py

--- IsolateTrend.py
+++ IsolateTrend.py
@@ -4,7 +4,10 @@ from pandas.tseries.offsets import BDay
 from datetime import datetime
 import pandas as pd
 
-final_path = "filtered_df.csv"
+final_path = "E:\\filtered_df.csv"
+
+path = "E:\\merged CSVs.csv"
+
 
 def value_to_float(x):
     if type(x) == float or type(x) == int:
@@ -29,34 +32,35 @@ def value_to_float(x):
     return 0.0
 
 def small_cap_rel_vol(date):
-    columns = ['SMA200', 'SMA50', 'SMA20', 'Open', 'Close', 'Market Cap', 'Change', 'Date', 'Ticker', 'Rel Volume']
+    columns = ['SMA200', 'SMA50', 'SMA20', 'Open', 'Close', 'Market Cap', 'Change', 'Date', 'Ticker', 'Rel Volume', 'RSI (14)', 'Industry']
 
     df = pd.read_csv(final_path, usecols=columns)
 
-    df['Market Cap'] = df['Market Cap'].apply(value_to_float)
+    # df['Market Cap'] = df['Market Cap'].apply(value_to_float)
+
+    df = df[df.Industry != 'Exchange Traded Fund']
+    df = df[df.Industry != 'Shell Companies']
 
     df = df[df['Date'] == date]
     df = df.replace("-", np.nan)
 
-    # df = df[df['Rel Volume'].astype(float) > 2]
+    df = df[df['Change'].astype(float) > 3]
+
     df = df[df['SMA200'].astype(float) < df['Close']]
     df = df[df['SMA50'].astype(float) < df['Close']]
-    df = df[df['Open'].astype(float) < df['Close']]
 
-    # df = df[df['SMA50'].between(df['Open'], df['Close'])]
     df = df[df['SMA20'].between(df['Open'], df['Close'])]
+
+    df = df[df['Open'].astype(float) < df['Close']]
 
     # remove % from all specified columns to get the raw value
     df['Change'] = df['Change'].replace({'%':''}, regex=True)
 
-    df = df[df['Market Cap'].astype(float) < 2000000000]
-    df = df[df['Change'].astype(float) > 5]
-
-    tickers = df['Ticker'].to_list()
+    # df = df[df['Market Cap'].astype(float) < 300000000]
 
     df['Change'] = df['Change'].fillna(0).astype('float')
 
-    df['Change'].apply(np.floor)
+    tickers = df['Ticker'].to_list()
 
     df.to_csv("Results.csv", encoding='utf-8')
 
@@ -77,24 +81,44 @@ def get_next_day(tickers, date):
 
     df.to_csv("Results_two.csv", encoding='utf-8')
 
+    return df['Ticker'].to_list()
 
-first_date = datetime.strptime('2021-06-04', "%Y-%m-%d").date()
+
+first_date = datetime.strptime('2021-06-03', "%Y-%m-%d").date()
+
+winner = 0
+loser = 0
+
+winner_sum = 0
+loser_sum = 0
+
+date_dup = []
 
 for i in range(40):
     try:
         date_one = str((first_date + relativedelta(days=+i) - BDay(1)).date())
-        date_two = str((first_date + relativedelta(days=+i+7) - BDay(1)).date())
+        date_two = str((first_date + relativedelta(days=+i+1) - BDay(1)).date())
 
-        if date_one not in date_two:
+        if date_one not in date_two and [date_one, date_two] not in date_dup:
+
+           date_dup.append([date_one, date_two])
 
            tickers = small_cap_rel_vol(date_one)
-           get_next_day(tickers, date_two)
+
+           tickers_two = get_next_day(tickers, date_two)
 
            df = pd.read_csv("Results.csv")
            df_two = pd.read_csv("Results_two.csv")
 
            close_one = df['Close'].tolist()
            close_two = df_two['Close'].tolist()
+
+           if sum(close_one) < sum(close_two) and sum(close_one) != 0 and sum(close_two) - sum(close_one) < 200:
+                winner_sum+=sum(close_two) - sum(close_one)
+                winner+=1
+           elif sum(close_one) > sum(close_two) and sum(close_one) != 0 and sum(close_one) - sum(close_two) < 200:
+               loser_sum+=sum(close_one) - sum(close_two)
+               loser+=1
 
            winners = open("winners2.txt", "a")
 
@@ -103,7 +127,8 @@ for i in range(40):
 
            winners.close()
 
-           print(str(sum(close_one)), str(sum(close_two)))
+           print(str(sum(close_one)), str(sum(close_two)), "(" + str(winner_sum) + ")", "(" + str(loser_sum) + ")",
+                 winner, loser, tickers_two, date_one, date_two)
 
     except Exception as err:
         print(err)
