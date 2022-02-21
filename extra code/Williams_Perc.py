d92ab109adf62920ee893b4363037c24b1143c0b
JeremyHudson43 <jer4349@gmail.com>
2022-02-21 17:51:20 -0500

    Update Williams_Perc.py

--- extra code/Williams_Perc.py
+++ extra code/Williams_Perc.py
@@ -18,30 +18,6 @@ def get_wr(high, low, close, lookback):
     return wr
 
 
-def rsi_func(df, periods=2, ema=False):
-    """
-    Returns a pd.Series with the relative strength index.
-    """
-    close_delta = df['close'].diff()
-
-    # Make two series: one for lower closes and one for higher closes
-    up = close_delta.clip(lower=0)
-    down = -1 * close_delta.clip(upper=0)
-
-    if ema == True:
-        # Use exponential moving average
-        ma_up = up.ewm(com=periods - 1, min_periods=periods).mean()
-        ma_down = down.ewm(com=periods - 1, in_periods=periods).mean()
-    else:
-        # Use simple moving average
-        ma_up = up.rolling(window=periods).mean()
-        ma_down = down.rolling(window=periods).mean()
-
-    rsi = ma_up / ma_down
-    rsi = 100 - (100 / (1 + rsi))
-
-    return rsi
-
 # get 2 day data from IB API
 for x in stock_list:
     try:
@@ -66,12 +42,9 @@ for x in stock_list:
             ))
 
         williams_perc = get_wr(market_data['high'], market_data['low'], market_data['close'], 2).iloc[-1]
-        rsi = rsi_func(market_data, periods=2, ema=False).iloc[-1]
-
         market_data['Williams %'] = williams_perc
-        market_data['RSI'] = rsi
 
-        if williams_perc < -80 and rsi < 10:
+        if williams_perc < -90:
             market_data.to_csv(f'C:\\Users\\Frank Einstein\\PycharmProjects\\Williams_Alert\\above_ma_ETF\\{stock}.csv')
 
     except Exception as err:
@@ -103,12 +76,10 @@ for x in stock_list:
             ))
 
         williams_perc = get_wr(market_data['high'], market_data['low'], market_data['close'], 2).iloc[-1]
-        rsi = rsi_func(market_data, periods=2, ema=False).iloc[-1]
 
         market_data['Williams %'] = williams_perc
-        market_data['RSI'] = rsi
 
-        if williams_perc > -20 and rsi > 90:
+        if williams_perc > -10:
             market_data.to_csv(f'C:\\Users\\Frank Einstein\\PycharmProjects\\Williams_Alert\\below_ma_ETF\\{stock}.csv')
 
     except Exception as err:
@@ -140,12 +111,10 @@ for x in stock_list:
             ))
 
         williams_perc = get_wr(market_data['high'], market_data['low'], market_data['close'], 2).iloc[-1]
-        rsi = rsi_func(market_data, periods=2, ema=False).iloc[-1]
 
         market_data['Williams %'] = williams_perc
-        market_data['RSI'] = rsi
 
-        if williams_perc < -80 and rsi < 10:
+        if williams_perc < -90:
             market_data.to_csv(f'C:\\Users\\Frank Einstein\\PycharmProjects\\Williams_Alert\\above_ma\\{stock}.csv')
 
     except Exception as err:
@@ -177,12 +146,10 @@ for x in stock_list:
             ))
 
         williams_perc = get_wr(market_data['high'], market_data['low'], market_data['close'], 2).iloc[-1]
-        rsi = rsi_func(market_data, periods=2, ema=False).iloc[-1]
 
         market_data['Williams %'] = williams_perc
-        market_data['RSI'] = rsi
 
-        if williams_perc > -20 and rsi > 90:
+        if williams_perc > -10:
             market_data.to_csv(f'C:\\Users\\Frank Einstein\\PycharmProjects\\Williams_Alert\\below_ma\\{stock}.csv')
 
     except Exception as err:
