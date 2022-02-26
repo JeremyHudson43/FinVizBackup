a7368f596b2b9aa9cc4965a2b2a10476a265fe5f
JeremyHudson43 <jer4349@gmail.com>
2022-02-26 12:54:56 -0500

    Update Set_Orders.py

--- extra code/Set_Orders.py
+++ extra code/Set_Orders.py
@@ -5,7 +5,6 @@ import time
 import pandas as pd
 
 ib = IB()
-
 ib.connect('127.0.0.1', 7497, clientId=random.randint(0, 300))
 
 def sleep_until_market_open():
@@ -19,33 +18,34 @@ def sleep_until_market_open():
         time.sleep(time_until_market_open)
 
 
+def place_order(ticker, year, month, day, strike, right):
+
+    contract = Option(ticker, year + month + day, strike, right, "SMART")
+
+    ib.qualifyContracts(contract)
+    contract_data = ib.reqTickers(*[contract])[0]
+
+    bid = contract_data.bid
+
+    qty = 1
+
+    limit_price = bid * 1.03
+    take_profit = bid * 1.15
+    stop_loss_price = bid * 0.5
+
+    print(bid)
+
+    buy_order = ib.bracketOrder(
+               'BUY',
+               quantity=qty,
+               limitPrice=limit_price,
+               takeProfitPrice=take_profit,
+               stopLossPrice=stop_loss_price
+           )
+
+    for o in buy_order:
+        ib.placeOrder(contract, o)
+
+
 sleep_until_market_open()
-
-year_one = '2022'
-month_one = '02'
-day_one = '28'
-
-contract = Option("SPY", year_one + month_one + day_one, 437, "P", "SMART")
-ib.qualifyContracts(contract)
-contract_data = ib.reqTickers(*[contract])[0]
-
-bid = contract_data.bid
-
-qty_one = 1
-
-limit_price = bid * 1.02
-take_profit = bid * 1.15
-stop_loss_price = bid * 0.5
-
-print(bid)
-
-buy_order_one = ib.bracketOrder(
-           'BUY',
-           quantity=qty_one,
-           limitPrice=limit_price,
-           takeProfitPrice=take_profit,
-           stopLossPrice=stop_loss_price
-       )
-
-for o in buy_order_one:
-    ib.placeOrder(contract, o)
+place_order('SPY', '2022', '02', '28', '437', 'P')
