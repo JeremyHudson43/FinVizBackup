a96880db3a145243f5901bf1b3a960817743ec11
JeremyHudson43 <jer4349@gmail.com>
2022-03-02 10:35:52 -0500

    Update Williams_Perc_Two.py

--- extra code/Williams_Perc_Two.py
+++ extra code/Williams_Perc_Two.py
@@ -154,23 +154,12 @@ stock_list_two = Screener(filters=['ta_sma200_pb', 'sh_avgvol_o500', 'sh_price_o
                            table='Performance', order='price')
 iterate(stock_list_two, path, williams_two, rsi_two, 'put', True)
 
-# stock_list_three = Screener(filters=['ta_sma200_pa', 'sh_avgvol_o500', 'sh_price_o1', 'ind_stocksonly', 'sh_opt_option'],
-#                             table='Performance', order='price')
-# iterate(stock_list_three, path, williams_one, rsi_one, 'call', False)
-
-# stock_list_four = Screener(filters=['ta_sma200_pb', 'sh_avgvol_o500', 'sh_price_o1', 'ind_stocksonly' ,'sh_opt_option'],
-#                            table='Performance', order='price')
-# iterate(stock_list_four, path, williams_two, rsi_two, 'put', False)
 
 try:
 
     df = pd.concat(map(functools.partial(pd.read_csv, encoding='latin-1', compression=None, error_bad_lines=False),
                        glob.glob(both + "/*.csv")))
     df.to_csv(f'{path}\\combined_both.csv', index=False)
-
-    df = pd.concat(map(functools.partial(pd.read_csv, encoding='latin-1', compression=None, error_bad_lines=False),
-                       glob.glob(either + "/*.csv")))
-    df.to_csv(f'{path}\\combined_either.csv', index=False)
 
 except Exception as err:
     print(traceback.format_exc())
