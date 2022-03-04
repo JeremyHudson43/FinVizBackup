244adadd505bb4cededc6d2c1ef80dd7e859304c
JeremyHudson43 <jer4349@gmail.com>
2022-03-04 09:19:11 -0500

    Update Set_Orders.py

--- extra code/Set_Orders.py
+++ extra code/Set_Orders.py
@@ -20,6 +20,8 @@ def sleep_until_market_open():
 
 
 def place_order(ticker, year, month, day, strike, right, qty):
+    
+    acc_vals = float([v.value for v in ib.accountValues() if v.tag == 'CashBalance' and v.currency == 'USD'][0])
 
     contract = Option(ticker, year + month + day, strike, right, "SMART")
 
@@ -30,35 +32,40 @@ def place_order(ticker, year, month, day, strike, right, qty):
     ask = contract_data.ask
 
     mid = (bid + ask) / 2
-    mid = ask
+    
+    if (mid * 100) > (acc_vals * 0.2):
+        print(ticker + ' option too big for account')
+        
+    else:
 
-    limit_price = mid
-    take_profit = mid * 1.20
-    stop_loss_price = mid * 0.50
+        limit_price = mid
+        take_profit = mid * 1.20
+        stop_loss_price = mid * 0.50
+    
+        limit_price = 0.05 * round(limit_price / 0.05)
+        take_profit = 0.05 * round(take_profit / 0.05)
+        stop_loss_price = 0.05 * round(stop_loss_price / 0.05)
+    
+        limit_price = round(limit_price, 2)
+        take_profit = round(take_profit, 2)
+        stop_loss_price = round(stop_loss_price, 2)
+    
+        buy_order = ib.bracketOrder(
+                   'BUY',
+                   quantity=qty,
+                   limitPrice=limit_price,
+                   takeProfitPrice=take_profit,
+                   stopLossPrice=stop_loss_price
+               )
+    
+        for o in buy_order:
+           o.tif = 'GTC'
+           ib.sleep(0.00001)
+           ib.placeOrder(contract, o)
 
-    limit_price = 0.05 * round(limit_price / 0.05)
-    take_profit = 0.05 * round(take_profit / 0.05)
-    stop_loss_price = 0.05 * round(stop_loss_price / 0.05)
 
-    limit_price = round(limit_price, 2)
-    take_profit = round(take_profit, 2)
-    stop_loss_price = round(stop_loss_price, 2)
+sleep_until_market_open()
 
-    buy_order = ib.bracketOrder(
-               'BUY',
-               quantity=qty,
-               limitPrice=limit_price,
-               takeProfitPrice=take_profit,
-               stopLossPrice=stop_loss_price
-           )
-
-    for o in buy_order:
-        o.tif = 'GTC'
-        ib.sleep(0.001)
-        ib.placeOrder(contract, o)
-
-
-# sleep_until_market_open()
-
-# place_order('SPY', '2022', '03', '11', '438', 'P', 2)
-place_order('UVXY', '2022', '03', '11', '18', 'P', 1)
+place_order('CARR', '2022', '03', '18', '43', 'P', 2)
+place_order('PARA', '2022', '03', '18', '35', 'P', 1)
+place_order('WMT', '2022', '03', '18', '135', 'P', 1)
