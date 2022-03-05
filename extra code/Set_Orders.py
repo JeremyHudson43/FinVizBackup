0ab283ad66b0ed6bb442aea4a52d1ba831e79f12
JeremyHudson43 <jer4349@gmail.com>
2022-03-05 11:48:49 -0500

    Update Set_Orders.py

--- extra code/Set_Orders.py
+++ extra code/Set_Orders.py
@@ -8,6 +8,15 @@ from dateutil import parser
 ib = IB()
 ib.connect('127.0.0.1', 7497, clientId=random.randint(0, 300))
 
+
+def get_percent(first, second):
+    if first == 0 or second == 0:
+        return 0
+    else:
+        percent = first / second * 100
+    return percent
+
+
 def sleep_until_market_open():
     now = datetime.now()  # time object
 
@@ -20,7 +29,7 @@ def sleep_until_market_open():
 
 
 def place_order(ticker, year, month, day, strike, right, qty):
-    
+
     acc_vals = float([v.value for v in ib.accountValues() if v.tag == 'CashBalance' and v.currency == 'USD'][0])
 
     contract = Option(ticker, year + month + day, strike, right, "SMART")
@@ -32,24 +41,24 @@ def place_order(ticker, year, month, day, strike, right, qty):
     ask = contract_data.ask
 
     mid = (bid + ask) / 2
-    
-    if (mid * 100) > (acc_vals * 0.2):
+
+    if (mid * 100) > (acc_vals * 0.2) and get_percent(bid, ask) < 25:
         print(ticker + ' option too big for account')
-        
+
     else:
 
         limit_price = mid
         take_profit = mid * 1.20
         stop_loss_price = mid * 0.50
-    
+
         limit_price = 0.05 * round(limit_price / 0.05)
         take_profit = 0.05 * round(take_profit / 0.05)
         stop_loss_price = 0.05 * round(stop_loss_price / 0.05)
-    
+
         limit_price = round(limit_price, 2)
         take_profit = round(take_profit, 2)
         stop_loss_price = round(stop_loss_price, 2)
-    
+
         buy_order = ib.bracketOrder(
                    'BUY',
                    quantity=qty,
@@ -57,7 +66,7 @@ def place_order(ticker, year, month, day, strike, right, qty):
                    takeProfitPrice=take_profit,
                    stopLossPrice=stop_loss_price
                )
-    
+
         for o in buy_order:
            o.tif = 'GTC'
            ib.sleep(0.00001)
@@ -65,6 +74,8 @@ def place_order(ticker, year, month, day, strike, right, qty):
 
 
 sleep_until_market_open()
+
+# ASHR,
 
 place_order('CARR', '2022', '03', '18', '43', 'P', 2)
 place_order('PARA', '2022', '03', '18', '35', 'P', 1)
