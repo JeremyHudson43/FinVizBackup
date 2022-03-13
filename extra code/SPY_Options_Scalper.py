f5f1f1e0ecd3a6027281480d499d134e823507a5
JeremyHudson43 <jer4349@gmail.com>
2022-03-13 11:25:47 -0400

    Update SPY_Options_Scalper.py

--- extra code/SPY_Options_Scalper.py
+++ extra code/SPY_Options_Scalper.py
@@ -74,7 +74,7 @@ def place_order(call, put, qty):
             ))
 
         two_hundred_ema = talib.EMA(market_data['close'].values, timeperiod=200).iloc[-2]
-        one_hundred_ema = talib.EMA(market_data['close'].values, timeperiod=100).iloc[-2]
+        fifty_ema = talib.EMA(market_data['close'].values, timeperiod=50).iloc[-2]
         ten_ema = talib.SMA(market_data['close'].values, timeperiod=10).iloc[-2]
 
         last_close = market_data['close'].iloc[-2]
@@ -89,17 +89,17 @@ def place_order(call, put, qty):
         print("\nLast Close: " + str(last_close) + '\n')
         print("Williams %: " + str(williams_perc) + '\n')
         print('200 EMA: ' + str(two_hundred_ema) + '\n')
-        print('100 EMA: ' + str(one_hundred_ema) + '\n')
+        print('50 EMA: ' + str(fifty_ema) + '\n')
         print("10 EMA: " + str(ten_ema) + '\n')
         print('- - - - - - - - - - - - - - - - - - - - \n')
 
-        if williams_perc <= -95 and last_close > two_hundred_ema and last_close > one_hundred_ema\
+        if williams_perc <= -95 and last_close > two_hundred_ema and last_close > fifty_ema\
                 and last_close > ten_ema:
             extreme_value = True
             contract = call
 
         elif williams_perc >= -5 and\
-                last_close < two_hundred_ema and last_close < one_hundred_ema\
+                last_close < two_hundred_ema and last_close < fifty_ema\
                 and last_close < ten_ema:
             extreme_value = True
             contract = put
